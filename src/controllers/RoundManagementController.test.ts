708c6150ede54b5999c7e423328d3f2375f48c64
DPodolniy <danilpodolniy@gmail.com>
2024-05-02 15:39:31 +0300

    some additions

--- src/controllers/RoundManagementController.test.ts
+++ src/controllers/RoundManagementController.test.ts
@@ -10,71 +10,112 @@ describe('RoundManagementController', () => {
     rMC = new RoundManagementController(new RoundManagementState());
   });
 
-  /*
-    2.1. Старт раунда
-      Сразу же после объявления начала раунда запускается 10-и секундный таймер.
-      2.1.1. Значение таймера не может быть отрицательным, при достижении нуля таймер должен останавливаться.
-      2.1.2. Значение таймера при старте == захардкоженный START_TIME.
-      2.1.3. Значение таймера уменьшается тиками на одну секунду, следовательно после каждого тика значение таймера падает ровно на одну секунду.
-  */
-  test('Timer works correctly', () => {
-    rMC.startTimer();
-    expect(rMC.roundState.timeForBetting).toBe(
-      RoundManagementController.START_TIME,
-    );
+  describe('Timer works correctly', () => {
+    it('Should decrease timeForBetting 1 per tick and should not decrease timeForBetting if its value is 0', () => {
+      rMC.startTimer();
+      expect(rMC.roundState.timeForBetting).toBe(
+        RoundManagementController.START_TIME,
+      );
 
-    for (let tick = 10; tick > 0; tick--) {
-      expect(rMC.roundState.timeForBetting).toBe(tick);
-      expect(rMC.isTimeEnded()).toBe(false);
+      for (let tick = 10; tick > 0; tick--) {
+        expect(rMC.roundState.timeForBetting).toBe(tick);
+        expect(rMC.isTimeEnded()).toBe(false);
+        rMC.tick();
+      }
+
       rMC.tick();
-    }
-
-    rMC.tick();
-    expect(rMC.roundState.timeForBetting).toBe(0);
-    expect(rMC.isTimeEnded()).toBe(true);
+      rMC.tick();
+      expect(rMC.roundState.timeForBetting).toBe(0);
+      expect(rMC.isTimeEnded()).toBe(true);
+    });
   });
 
-  /*
-    2.1. Старт раунда
-      2.1.4. После начала раунда его статус == SETTING_BETS
-      2.1.5. После окончания таймера статус раунда == WAITING_FOR_RESULTS
-    2.2. Завершение раунда
-      2.2.1. После получения значений, статус раунда == WAITING FOR RESULTS
-    2.3. Отмена раунда
-      2.3.2. После отмены раунда, статус раунда == WAITING FOR RESULTS
-  */
-  test('State transitions work correctly', async () => {
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.GETTING_CURRENT_ROUND);
-    await rMC.startRound();
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.SETTING_BETS);
+  describe('IsTimeEnded func works correctly', () => {
+    it('Should return false when time for betting is 10', () => {
+      rMC.startTimer();
+      expect(rMC.isTimeEnded()).toBe(false);
+    });
 
-    await rMC.closeRound();
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_FOR_RESULTS);
+    it('Should return true when time for betting is 0', () => {
+      rMC.startTimer();
 
-    await rMC.finishRound('1', '6');
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
-
-    // just in case
-    expect(rMC.roundState.errorMessage).toBe('');
-
-    await rMC.startRound();
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.SETTING_BETS);
-
-    await rMC.cancelRound();
-    expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
-
-    // just in case
-    expect(rMC.roundState.errorMessage).toBe('');
+      for (let tick = 10; tick > 0; tick--) {
+        expect(rMC.roundState.timeForBetting).toBe(tick);
+        expect(rMC.isTimeEnded()).toBe(false);
+        rMC.tick();
+      }
+      expect(rMC.isTimeEnded()).toBe(true);
+    });
   });
 
-  /*
-  2.3. Отмена раунда
-    2.3.1. Если после отмены получены результаты, должно выводиться корректное сообщение об ошибке.
-  */
-  test('Canceled round can not be finished', async () => {
-    await rMC.startRound();
-    await rMC.cancelRound();
-    await rMC.finishRound('2', '4');
-    expect(rMC.roundState.errorMessage).toBe(locales.errorFinish);
+  describe('State transitions work correctly', () => {
+    it('State transitions work correctly', async () => {
+      expect(rMC.roundState.stateOfRound).toBe(
+        RoundState.GETTING_CURRENT_ROUND,
+      );
+      await rMC.startRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.SETTING_BETS);
+
+      await rMC.closeRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_FOR_RESULTS);
+
+      await rMC.finishRound('1', '6');
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
+
+      // just in case
+      expect(rMC.roundState.errorMessage).toBe('');
+
+      await rMC.startRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.SETTING_BETS);
+
+      await rMC.cancelRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
+
+      // just in case
+      expect(rMC.roundState.errorMessage).toBe('');
+    });
+  });
+
+  describe('StartRound', () => {
+    it('Should start the round, update its state and start the timer', async () => {
+      await rMC.startRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.SETTING_BETS);
+      expect(rMC.roundState.timeForBetting).toBeGreaterThan(0);
+    });
+  });
+
+  describe('CloseRound', () => {
+    it('Should close the round and reset the timer', async () => {
+      await rMC.startRound();
+      await rMC.closeRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_FOR_RESULTS);
+      expect(rMC.roundState.timeForBetting).toBe(-1);
+    });
+  });
+
+  describe('FinishRound', () => {
+    it('Should finish the round and reset the tiner', async () => {
+      await rMC.startRound();
+      await rMC.closeRound();
+      await rMC.finishRound('2', '4');
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
+      expect(rMC.roundState.timeForBetting).toBe(-1);
+    });
+
+    it('Canceled round can not be finished', async () => {
+      await rMC.startRound();
+      await rMC.cancelRound();
+      await rMC.finishRound('2', '4');
+      expect(rMC.roundState.errorMessage).toBe(locales.errorFinish);
+    });
+  });
+
+  describe('CancelRound', () => {
+    it('Should cancel the round and reset timer', async () => {
+      await rMC.startRound();
+      await rMC.cancelRound();
+      expect(rMC.roundState.stateOfRound).toBe(RoundState.WAITING_NEW_ROUND);
+      expect(rMC.roundState.timeForBetting).toBe(-1);
+    });
   });
 });
